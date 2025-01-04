80f0ca71594a63b9de6f6f68535f16d97ea87d05
K1ngHandy <bolds-slicks0g@icloud.com>
2025-01-04 00:39:41 -0500

    use effects

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,17 +7,41 @@ export default function Details(props) {
   const [details, setDetails] = useState(null)
 
   // 👉 TASK 4 - Create a side effect 🥇 that runs only after first render.
+  useEffect(() => {
+    console.log('Details component mounted');
+    }, []);
 
   // 👉 TASK 5 - Create a side effect 👻 that runs only after first render
   // and puts a 'click' event handler on document.
   // See what happens if we don't clean up.
+  useEffect(() => {
+    console.log('Details component mounted');
+    const handleClick = () => {
+      console.log('clicked');
+    }
+    document.addEventListener('click', handleClick);
+    return () => {
+      document.removeEventListener('click', handleClick);
+    }
+  });
 
   // 👉 TASK 6 - Create a side effect 🥵 that runs after every render.
+  useEffect(() => {
+    console.log('Details component rendered');
+  });
 
   // 👉 TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
   // Whenever props.friendId updates we should trigger a fetch for details of the friend.
   // The URL should end up looking like `http://localhost:4000/friends/1?api_key=xyz`
   // On success, shove the details of the friend in `details` slice of state
+  useEffect(() => {
+    axios.get(`${BASE_URL}/friends/${friendId}?api_key=${API_KEY}`)
+      .then(res => {
+        setDetails(res.data)
+      }).catch(err => {
+        console.error(err)
+      })
+  }, [friendId]);
 
   return (
     <div className='container'>
---
+++ src/components/Friend.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Friend = (props) => {
+    return (
+        <div className="friend">
+            {props.info.name}
+            <button onClick={() => props.openDetails(props.info.id)}>See details</button>
+        </div>
+    )
+}
+
+export default Friend;
